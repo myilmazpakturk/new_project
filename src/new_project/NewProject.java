fafaafc419e223c757d94ae8a54f4de2a3674036
Mustafa <myilmazpakturk@gmail.com>
2020-06-20 23:17:17 -0400

    ikinci surum

--- src/new_project/NewProject.java
+++ src/new_project/NewProject.java
@@ -5,6 +5,7 @@ public class NewProject {
 	public static void main(String[] args) {
 		System.out.println("merhaba asker");
 		System.out.println("saol");
+		System.out.println("tekrar merhaba asker");
 
 	}
 
